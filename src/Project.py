560c6d85de2dd97fee45754e31afef2ee618ff4e
KadirTaban <kadirtaban08@gmail.com>
2021-03-05 02:45:07 +0300

    cleanCode:)

--- src/Project.py
+++ src/Project.py
@@ -5,14 +5,15 @@ a = datetime.datetime.now()
 video_capture_0=cv2.VideoCapture(0)
 path = "video.mp4"
 video_capture_1 = cv2.VideoCapture(path)
-b = datetime.datetime.now()
-print(b - a)
 
 while True:
     ret0, frame0= video_capture_0.read()
     ret1, frame1 = video_capture_1.read()
 
     if(ret0):
+        b = datetime.datetime.now()
+        print(b - a)
+
         cv2.imshow('Cam0',frame0)
 
     if(ret1):
