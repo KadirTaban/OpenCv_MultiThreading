04a8c1e4e048910675a93bc4fe6b7d978e634341
KadirTaban <kadirtaban08@gmail.com>
2021-03-04 21:24:04 +0300

    cleancode:)

--- src/project_mutlithreading.py
+++ src/project_mutlithreading.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-import threading
+from threading import Thread
 import datetime
 
 def function_1():
@@ -10,7 +10,7 @@ def function_1():
         ret0,frame0=video_capture_0.read()
         if(ret0):
             cv2.imshow("cam0",frame0)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            if cv2.waitKey(56) & 0xFF == ord('q'):
                 break
         else:
             break
@@ -19,13 +19,14 @@ def function_1():
 
 
 def function_2():
-    video_capture_1 = cv2.VideoCapture(1)
+    path="video.mp4"
+    video_capture_1 = cv2.VideoCapture(path)
 
     while True:
         ret1,frame1=video_capture_1.read()
         if(ret1):
             cv2.imshow("cam1",frame1)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            if cv2.waitKey(56) & 0xFF == ord('q'):
                 break
         else:
             break
