2275541453456938a6554ac9dafc3054e7fe21ae
KadirTaban <kadirtaban08@gmail.com>
2021-03-04 19:35:41 +0300

    cleancode:)

--- src/project_mutlithreading.py
+++ src/project_mutlithreading.py
@@ -2,25 +2,36 @@ import numpy as np
 import cv2
 import threading
 import datetime
-video_capture_1=cv2.VideoCapture(1)
-video_capture_0 = cv2.VideoCapture(0)
+
 def function_1():
+    video_capture_0 = cv2.VideoCapture(0)
 
     while True:
         ret0,frame0=video_capture_0.read()
         if(ret0):
             cv2.imshow("cam0",frame0)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
             break
+    video_capture_0.release()
+    cv2.destroyAllWindows()
+
 
 def function_2():
+    video_capture_1 = cv2.VideoCapture(1)
 
     while True:
         ret1,frame1=video_capture_1.read()
         if(ret1):
             cv2.imshow("cam1",frame1)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
             break
+    video_capture_1.release()
+    cv2.destroyAllWindows()
+
 
 t1=threading.Thread(target=function_1)
 t2=threading.Thread(target=function_2)
@@ -29,6 +40,3 @@ t1.start()
 t2.start()
 
 
-video_capture_0.release()
-video_capture_1.release()
-cv2.destroyAllWindows()
