956249643508beada40c989f5e46c68ac1c1a6b6
KadirTaban <kadirtaban08@gmail.com>
2021-03-04 18:46:06 +0300

    mt

--- src/project_mutlithreading.py
+++ src/project_mutlithreading.py
@@ -2,36 +2,31 @@ import numpy as np
 import cv2
 import threading
 import datetime
-a = datetime.datetime.now()
-video_capture_0=cv2.VideoCapture(0)
-video_capture_1=cv2.VideoCapture(2)
-def cam1():
-    while True:
+video_capture_1=cv2.VideoCapture(1)
+video_capture_0 = cv2.VideoCapture(0)
+def function_1():
 
-        ret0, frame0 = video_capture_0.read()
+    while True:
+        ret0,frame0=video_capture_0.read()
         if(ret0):
-            cv2.imshow('Cam0',frame0)
+            cv2.imshow("cam0",frame0)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-def cam2():
+def function_2():
+
     while True:
-
-        ret1, frame1 = video_capture_1.read()
+        ret1,frame1=video_capture_1.read()
         if(ret1):
-            cv2.imshow('Cam1',frame1)
-t1=threading.Thread(target=cam1)
-t2=threading.Thread(target=cam2)
-while True:
+            cv2.imshow("cam1",frame1)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+t1=threading.Thread(target=function_1)
+t2=threading.Thread(target=function_2)
 
-t1.start()
 t2.start()
-
-
-
+t1.start()
 video_capture_0.release()
 video_capture_1.release()
-
 cv2.destroyAllWindows()
-
